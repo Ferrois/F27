5e2a45e490dce8ecaa23f4b4036877a86107a1d3
Ferrois <ferrois2005@gmail.com>
2025-12-11 14:34:43 +0800

    CHANGE: Render to Heroku server

--- client/src/config.js
+++ client/src/config.js
@@ -1,12 +1,15 @@
 const isProduction = import.meta.env.PROD || import.meta.env.VITE_NODE_ENV === 'production';
 
+const renderLink = "https://resq-f27.onrender.com";
+const herokuLink = "https://resq-api-f0aa1cfc88e2.herokuapp.com/";
+
 export const config = {
   isProduction,
   API_URL: isProduction 
-    ? 'https://resq-f27.onrender.com' 
+    ? herokuLink
     : (import.meta.env.VITE_API_URL || 'http://localhost:8080'),
   SOCKET_URL: isProduction 
-    ? 'https://resq-f27.onrender.com' 
+    ? herokuLink
     : (import.meta.env.VITE_SOCKET_URL || import.meta.env.VITE_API_URL || 'http://localhost:8080'),
 };
 
